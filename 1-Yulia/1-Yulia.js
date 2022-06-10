e39f2c0820aa8ec3cca1da055a4f8f950b52aa6b
uliaslatvitckaa <yuli.slat@yandex.ru>
2022-06-10 18:20:01 +0200

    1 and 2 javascript

---
+++ 1-Yulia/1-Yulia.js
@@ -0,0 +1,23 @@
+
+//первое задание (комент комонд+?)
+// let userName = 'LeoTolstoy';
+let bonusBalance = 131;
+// // console.log(userName)
+// console.log(bonusBalance)
+let ballsPlus = 50 * 5;
+let ballsMinus = 30;
+bonusBalance = bonusBalance + (ballsPlus - ballsMinus)
+console.log(bonusBalance)
+// task 3
+// let bonusBalance = 131;
+// let oneKilo = 80;
+// let rest = bonusBalance % oneKilo;
+// console.log(rest)
+// bonusBalance = bonusBalance - rest;
+// let maxKilo = bonusBalance / oneKilo;
+// console.log(maxKilo)
+
+
+
+
+
