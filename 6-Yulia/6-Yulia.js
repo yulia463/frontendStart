7bdb6c6a7ac33de7bec97f73c6ba6a55b30b7921
uliaslatvitckaa <yuli.slat@yandex.ru>
2022-06-21 18:39:07 +0200

    7 javascript

--- 6-Yulia/6-Yulia.js
+++ 6-Yulia/6-Yulia.js
@@ -10,16 +10,20 @@
 // let randomInsuld = "у тебя " + randomBodyPart + " еще более " + randomAbjective + " чем " + randomBodyAnimal + " у " + randomAnimal;
 // console.log(randomInsuld)
 
+//
+// let randomBodyParts = ["глаз", "нос", "череп"];
+// let randomAbjectives = ["красивая", "мягкая", "милая"];
+// let randomBodyAnimals = [" лапа ", " хвост ", " уши ", " усы "];
+// let randomAnimals = ["крыса", " слон ", " выхухоль "];
+// randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+// randomAbjective = randomAbjectives[Math.floor(Math.random() * 3)];
+// randomBodyAnimal = randomBodyAnimals[Math.floor(Math.random() * 4)];
+// randomAnimal = randomAnimals[Math.floor(Math.random() * 3)];
+// let randomInsuld = ["у тебя ", randomBodyPart, "еще более", randomAbjective, "чем", randomBodyAnimal, "у", randomAnimal].join(" ") + "!!!";
+// console.log(randomInsuld)
 
-let randomBodyParts = ["глаз", "нос", "череп"];
-let randomAbjectives = ["красивая", "мягкая", "милая"];
-let randomBodyAnimals = [" лапа ", " хвост ", " уши ", " усы "];
-let randomAnimals = ["крыса", " слон ", " выхухоль "];
-randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
-randomAbjective = randomAbjectives[Math.floor(Math.random() * 3)];
-randomBodyAnimal = randomBodyAnimals[Math.floor(Math.random() * 4)];
-randomAnimal = randomAnimals[Math.floor(Math.random() * 3)];
-let randomInsuld = ["у тебя ", randomBodyPart, "еще более", randomAbjective, "чем", randomBodyAnimal, "у", randomAnimal].join(" ") + "!!!";
-console.log(randomInsuld)
 
+let namber = [1, 2, 3];
+let non = namber.join( " больше чем " );
+console.log(non)
 
---
+++ 7-Yulia/7-Yulia.js
@@ -0,0 +1,7 @@
+let scores = {
+    "maks" : 3 ,
+    "lida" : 5 ,
+    "senya" : 14
+};
+scores.maks = scores.maks + 1;
+console.log(scores.maks)
