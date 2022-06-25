863d6c1e89c709a604a95d26f6af92a760c4087f
uliaslatvitckaa <yuli.slat@yandex.ru>
2022-06-25 17:39:22 +0200

    9-objects

---
+++ 9-Yulia/9-Yulia.js
@@ -0,0 +1,17 @@
+// let scores = { };
+// scores["Даня"] = 5;
+// scores["Алла"] = 1;
+// scores["Федя"] = 23;
+// scores["Даня"] += 45;
+// scores["Алла"] +=28;
+// console.log(scores);
+
+
+let myCrazyObject = {
+    "name": "Нелепый объект",
+    "some array":[7,9, { purpose:"путаница", number:123},3.3],
+    "random animal":"Банановая акула"
+};
+console.log(myCrazyObject["some array"][2].number)
+
+
